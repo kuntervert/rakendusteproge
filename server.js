9b508df2a700ee3f5fea1fcbc1b028dccb1e9c2b
kuntervert <43129473+kuntervert@users.noreply.github.com>
2019-09-09 13:17:03 +0300

    Epood

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+const path = require("path")
 const PORT = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "static", "index.html")))
+
+app.use(express.static("static"))
 
 app.listen(process.env.PORT || PORT, () => {
   console.log("Server started", PORT);
