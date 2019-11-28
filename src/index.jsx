1159c7673871b33427783f49fc1ac7174af1813b
Xaxa Evert <kuntaevert@gmail.com>
2019-11-29 00:19:32 +0200

    Not Found page added

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom";
 import Header from "./components/Header.jsx";
 import HomePage from "./pages/HomePage.jsx";
 import ItemPage from "./pages/ItemPage.jsx";
-import {BrowserRouter, Route } from "react-router-dom";
+import {BrowserRouter, Route, Switch } from "react-router-dom";
 import LoginPage from "./pages/LoginPage.jsx";
 import SignupPage from "./pages/SignupPage.jsx";
-import UserPage from "./components/UserPage.jsx";
-
+import UserPage from "./pages/UserPage.jsx";
+import NotFound from "./pages/NotFound.jsx";
 
 class App extends React.Component{
     state = {
@@ -29,21 +29,28 @@ class App extends React.Component{
     render(){
         return(
             <BrowserRouter>
-            <Route
+                        <Route
              path={"/"}
              render= { (props) =>
               <Header {...props} 
               token={this.state.token}
               user={this.state.user}
               />} />
-            <Route path="/" exact component={HomePage}/>
-            <Route path="/items/:itemId" exact component={ItemPage}/>
-            <Route 
-            path="/login" 
-            exact
-            render={(props) => <LoginPage {...props} onLogin={this.handleLogin}/>}/>
+            <Switch>
+
+            <Route path="/"
+             exact component={HomePage}/>
+
+            <Route path="/items/:itemId" 
+            exact component={ItemPage}/>
+
+            <Route component={NotFound}/>
+
+            <Route path="/login" 
+            exact render={(props) => <LoginPage {...props} onLogin={this.handleLogin}/>}/>
             
             <Route path="/signup" exact component={SignupPage}/>
+
             <Route 
             path="/users/:userId"
             exact
@@ -51,6 +58,7 @@ class App extends React.Component{
                 return <UserPage{...props} user={this.state.user}/>;
             }}
             />
+        </Switch>
         </BrowserRouter>
         );
     }
