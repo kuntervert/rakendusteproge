0780e300e3e9826842a101ad24e67198b713fcee
Xaxa Evert <kuntaevert@gmail.com>
2019-12-01 16:34:58 +0200

    token deleted when  logged out and user redirected to home page

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import dataConsumer from "../components/dataConsumer.jsx";
+import redirecter from "../components/redirecter.jsx";
 
 class UserPage extends React.PureComponent {
     static propTypes = {
@@ -18,4 +19,4 @@ class UserPage extends React.PureComponent {
     }
 }
 
-export default dataConsumer(UserPage);
+export default dataConsumer(redirecter(UserPage));
